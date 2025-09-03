c1f6c104abdb0a878fc8e7c8168bb989c40ea66d
yxb123456 <2186256471@qq.com>
2025-09-03 13:05:21 +0800

    chore: :hammer: 删除一些无关依赖并发版

--- packages/Browser/Number/useRandomInteger/useRandomInteger.ts
+++ packages/Browser/Number/useRandomInteger/useRandomInteger.ts
@@ -1,5 +1,14 @@
+/**
+ *
+ * @param min 最小值
+ * @param max 最大值
+ * @returns 返回一个介于最小值与最大值之间的一个随机数;
+ */
 export function useRandomInteger(min: number, max: number) {
   if (min > max)
     throw new Error(`Min:${min} cannot be greater than max:${max}`)
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+// npx  tsx .\useRandomInteger.ts
+// const res = useRandomInteger(15, 20)
+// console.warn(res)
